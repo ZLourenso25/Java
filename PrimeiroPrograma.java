a5f3316b9f2ed3a8de4b0f0bf58b42be680b5355
Jose Lourenço M. Salviano <113633459+ZLourenso25@users.noreply.github.com>
2023-01-14 16:26:16 -0300

    Add files via upload

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package primeiroprograma;
+
+// * @author Jose Lourenço
+
+public class PrimeiroPrograma {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("olá, Mundo!!");
+    }
+}
