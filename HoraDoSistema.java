b66772e3f6b35f1333fad2441fd0f031e64f3aca
Jose Lourenço M. Salviano <113633459+ZLourenso25@users.noreply.github.com>
2023-01-16 21:51:20 -0300

    SecondProgram
    Segundo programa do curso de java pelo curso em video

---
+++ HoraDoSistema.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package aula4.horadosistema;
+
+import java.util.Date
+
+/**
+ *
+ * @author Jose
+ */
+public class HoraDoSistema {
+
+    public static void main(String[] args) {
+         Date relogio = new Date();
+         System.out.println("A hora do sistema é");
+         System.out.println(relogio.toString());
+    }
+}
